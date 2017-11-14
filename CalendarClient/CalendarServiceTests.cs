2c955fc463a7538618e27a0adafc21d7c75747d8
Slawomir Aleksiuk <slawomir@aleksiuk.pl>
2017-11-14 21:57:03 +0100

    Added proof of concept getting events from given calendar and creating a new event in one of them. Requires having a client_secret.json file in CalendarClient project. This file is intentionally ignored and not added to the repository for the security reasons

---
+++ CalendarClient/CalendarServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CalendarClient
+{
+    [TestFixture]
+    public class CalendarServiceTests
+    {
+        [Test]
+        public void ReadMeetings_CheckConsole()
+        {
+            var calendarService = new CalendarServiceClient();
+            calendarService.GetMeetings();
+        }
+
+        [Test]
+        public void AddMeeting_CheckConsole()
+        {
+            var calendarService = new CalendarServiceClient();
+            calendarService.AddMeeting();
+        }
+
+        [Test]
+        public void GetWholeHour()
+        {
+            var now = DateTime.Now;
+            Console.WriteLine(new DateTime(now.Ticks - now.Ticks % TimeSpan.TicksPerHour));
+        }
+    }
+}
