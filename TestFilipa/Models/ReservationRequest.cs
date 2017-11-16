dad2727b581ba35d6fd219b069d0d1992bf64dd9
Filip Czaja <filip.czaja@dfconline.com>
2017-11-16 15:09:06 +0100

    Test Filipa 03 - Web API

---
+++ TestFilipa/Models/ReservationRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestFilipa.Models
+{
+    public class ReservationRequest
+    {
+        public string StartReservation { get; set; }
+        public string StopReservation { get; set; }
+        public int NumberOfPersons { get; set; }
+    }
+}
